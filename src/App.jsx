3a94dbdb05b34d0b983a6579cd5c7049ef4679a0
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-05-17 16:59:12 +0200

    Implement react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import SuperHeroes from "./pages/SuperHeroes";
+import RQSuperHeroes from "./pages/RQSuperHeroes";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/super-heroes">SuperHeroes</Link>
+          </li>
+          <li>
+            <Link to="/rq-super-heroes">RQSuperHeroes</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/super-heroes" element={<SuperHeroes />} />
+        <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

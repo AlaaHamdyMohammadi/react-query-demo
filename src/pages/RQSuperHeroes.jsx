533a159b0207c4c847556828a3bf7d76d34b7dc4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-05-17 23:26:11 +0200

    Handle query error

--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -1,21 +1,29 @@
 /* eslint-disable no-unused-vars */
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
 
 const RQSuperHeroes = () => {
-  const {isLoading, data} = useQuery('super-heroes', () => {
-    return axios.get('http://localhost:4000/superheroes')
-  })
-  if(isLoading){
-    return <h2>Loading...</h2>
+  const { isLoading, data, isError, error } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes1");
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
   return (
     <div>
       <h2>RQSuperHeroes</h2>
-      {data?.data.map((hero) => <div key={hero.name}>{hero.name}</div>)}
+      {data?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
     </div>
   );
-}
+};
 
-export default RQSuperHeroes
+export default RQSuperHeroes;

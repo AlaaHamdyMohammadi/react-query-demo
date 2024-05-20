db643540472a5e30d879aec24e1953b7dfaa739f
Alaa Hamdy <alaahamdy2197@gmail.com>
2024-05-20 12:25:40 +0300

    add useQuery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useQuery } from 'react-query'
+
+//https://jsonplaceholder.typicode.com/todos
 
 const Home = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
   return (
-    <div>Home</div>
+    <div>{data.map(todo => <div key={todo.id}>{todo.title}</div>)}</div>
   )
 }
 
